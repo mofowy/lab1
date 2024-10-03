ca5200e9e2e2f3bbbbd802efec3a70c3073118a6
mofowy <volodiachkax@gmail.com>
2024-10-03 14:40:56 +0300

    lab2

--- functions/calcFunctions.py
+++ functions/calcFunctions.py
@@ -10,15 +10,16 @@ def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return "Error: Division by zero"
+    if b == 0:
+        raise ZeroDivisionError("Error: Division by zero")
+    return a / b
 
 def power(a, b):
     return a ** b
 
 def sqrt(a):
+    if a < 0:
+        raise ValueError("Error: Cannot take the square root of a negative number")
     return math.sqrt(a)
 
 def modulus(a, b):
